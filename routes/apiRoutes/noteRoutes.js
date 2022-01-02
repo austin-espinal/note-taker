b5062bdff7cecd5fcb9565f24d2f95ff5f43bd26
unknown <austinespi@gmail.com>
2022-01-01 19:54:08 -0500

    connecting save note to backend

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,19 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
 const router = require('express').Router();
 var uniqid = require('uniqid');
 const { notes } = require('../../db/db.json');
 
+//function to add new note to db.json
+function createNote(body, noteArray) {
+  const newNote = body;
+  noteArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, '../../db/db.json'),
+    JSON.stringify({ notes }, null, 2)
+  );
+  return newNote;
+}
+
+
 router.get('/api/notes', (req, res) => {
-  // let results = notes;
-  // if (req.query) {
-  //   results = filterByQuery(req.query, results);
-  // }
-  // res.json(results);
+  fs.readFile('../../db/db.json', 'utf8', (err, data => {
+    if (err) {
+      return console.error(err);
+    }
+    let results = data;
+    res.json(results);
+  }))
 });
 
 router.post('/api/notes', (req, res) => {
   // set id using uniqid npm package
   req.body.id = uniqid();
-
+  
+  const note = createNote(req.body, notes);
+  res.json(note);
 });
 
 module.exports = router;
